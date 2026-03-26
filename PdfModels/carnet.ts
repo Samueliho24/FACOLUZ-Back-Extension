71d3b73f3cc5ccddb1a33b3dfde72028cbae9b11
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2026-03-25 20:39:50 -0400

    Diseño de carnet

--- PdfModels/carnet.ts
+++ PdfModels/carnet.ts
@@ -7,9 +7,23 @@ export function buildCarnet(dataCallback: any, endCallback: any, data: ICarnet){
     doc.on('data', dataCallback);
     doc.on('end', endCallback);
 
-    doc.image(`/data/profilePics/${data.id}.png`, 50, 50, {width: 35});
-    doc.text(`${data.name} ${data.lastname}`)
-    doc.text(`${data.studentsIdentification}`)
+    //Recuadro para delimitar
+    doc.moveTo(50, 50)
+    .lineTo(50, 240)
+    .lineTo(203, 240)
+    .lineTo(203, 50)
+    .stroke();
+
+    doc.image("./assets/CardBanner.png", 50, 50, {width: 153})
+    doc.image(`/data/profilePics/${data.id}.png`, 100, 115, {width: 50, height: 55});
+    doc.text(`${data.name} ${data.lastname}`, 50, 175, {
+        width: 153,
+        align: 'center'
+    })
+    doc.text(`C.I.: ${data.studentsIdentification}`, 50, 190, {
+        width: 153,
+        align: 'center'
+    })
 
     doc.end();
 }
