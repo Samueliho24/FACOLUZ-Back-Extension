41f1d017e48ebcec5f71df09157516836dbb57ba
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2026-03-31 20:07:32 -0400

    Correcciones en queries de reporte diario

--- dbConnection/reports.ts
+++ dbConnection/reports.ts
@@ -7,18 +7,14 @@ export async function getReportInfo(start: Date, end: Date){
             i.id as invoiceId,
             i.status as invoiceStatus,
             i.billableitem,
-            i.changeRate,
             i.date,
             i.chargedAmount,
-            i.currencyReceived,
-            i.amountReceived,
-            i.currencyReturned,
             s.name,
             s.lastname,
             s.studentsidentification
         FROM invoices i
         JOIN students s
-        ON s.id = i.studentId
+        ON s.studentsIdentification = i.StudentIdentification
         WHERE i.date > ? AND i.date < ?
     `, [start, end])
     return res
