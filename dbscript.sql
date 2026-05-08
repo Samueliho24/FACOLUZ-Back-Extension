fc7f67ba56bab7878e861325905f4f6017096b38
Jesus Lozano <jesus.lozano@adm.interactiveworkers.com>
2026-05-08 11:26:34 -0400

    Correcciones a la tabla de usuarios

--- dbscript.sql
+++ dbscript.sql
@@ -331,7 +331,6 @@ CREATE TABLE `users` (
   `lastname` varchar(20) NOT NULL,
   `passwordSHA256` varchar(64) NOT NULL,
   `type` int(11) NOT NULL,
-  `identificationType` int(10) NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -339,12 +338,7 @@ CREATE TABLE `users` (
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES
-(1,'admin','admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',0,0,1),
-(2,'2','2','d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35',1,1,1),
-(3,'3','3','4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce',2,1,1),
-(4,'4','4','4b227777d4dd1fc61c6f884f48641d02b4d121d3fd328cb08b5531fcacdabf8a',3,1,1),
-(5,'5','5','ef2d127de37b942baad06145e54b0c619a1f22327b2ebbcfbec78f5564afe39d',4,1,1),
-(6,'6','6','e7f6c011776e8db7cd330b54174fd76f7d0216b612387a5ffcfb81e6f0919683',5,1,1);
+(1,'admin','admin','66908395b9edc2fbbedd85dbda476785736a14ac7313f06a362f779b643734e5',0,1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
